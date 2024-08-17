c67cfd508add10f1a9c873e1e6ad4ecc40eaa76e
apBUSampK <sdsnm737@gmail.com>
2024-08-17 17:44:40 +0300

    setting up state machine

--- scripts/attributor.gd
+++ scripts/attributor.gd
@@ -1,12 +1,24 @@
 extends RefCounted
 
 class Attributor:
+	# max hp
+	var mhp: float = 400.0
+	# damage
+	var dmg: float = 20
+	# attack speed
+	var aspd: float = 1
 	# aggressiveness
 	var agro: float = 0.0
+	# curiosity
+	var curious: float = .1
 	# movement speed
 	var speed: float = 1.0
+	# turning speed in radians
+	var phiSpeed: float = 2.0
 	# how brave the creature is
 	var guts: float = 0.0
+	# range of view
+	var vRange: float = 30
 	# field of view in radians
 	var fov: float = PI/2
 	# radius in which the creature can sense other creatures even outside fov
